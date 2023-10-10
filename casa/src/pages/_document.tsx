3c60e3eeef31e43c7415f0c2907656f54f411e5e
Khanh Duy <khanhduycb1510@gmail.com>
2023-10-10 11:09:48 +0700

    meta tag update

--- casa/src/pages/_document.tsx
+++ casa/src/pages/_document.tsx
@@ -12,6 +12,20 @@ export default function Document() {
         <meta name="msapplication-TileColor" content="#da532c"/>
         <meta name="theme-color" content="#ffffff"/>
         <meta name="keywords" content="nội thất sang trọng, bàn ghế tarno, tarno, ván gỗ, vỉ gỗ, tủ đầu giường, tủ đầu giường thông minh, phòng ngủ hiện đại, nội thất hiện đại"/>
+        <meta name="description" content="Tại CASA, chúng tôi tự hào về việc biến không gian sống thành các tác phẩm nghệ thuật với các sản phẩm nội thất tinh xảo và thời thượng của chúng tôi. Nằm chặt trong trái tim của Việt Nam, chúng tôi đã dành nhiều năm để hoàn thiện nghệ thuật chế tác các tác phẩm vượt thời gian kết hợp hoàn hảo giữa tính thực dụng và vẻ đẹp." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://casa-vietnam.vn/" />
+        <meta property="og:title" content="CASA - Cuộc Sống Tiện Nghi" />
+        <meta property="og:description" content="Tại CASA, chúng tôi tự hào về việc biến không gian sống thành các tác phẩm nghệ thuật với các sản phẩm nội thất tinh xảo và thời thượng của chúng tôi. Nằm chặt trong trái tim của Việt Nam, chúng tôi đã dành nhiều năm để hoàn thiện nghệ thuật chế tác các tác phẩm vượt thời gian kết hợp hoàn hảo giữa tính thực dụng và vẻ đẹp." />
+        <meta property="og:image" content="https://casa-vietnam.vn/assets/3d-rendering-contemporary-modern-dining-room-living-room-with-luxury-decor.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://casa-vietnam.vn/" />
+        <meta property="twitter:title" content="CASA - Cuộc Sống Tiện Nghi" />
+        <meta property="twitter:description" content="Tại CASA, chúng tôi tự hào về việc biến không gian sống thành các tác phẩm nghệ thuật với các sản phẩm nội thất tinh xảo và thời thượng của chúng tôi. Nằm chặt trong trái tim của Việt Nam, chúng tôi đã dành nhiều năm để hoàn thiện nghệ thuật chế tác các tác phẩm vượt thời gian kết hợp hoàn hảo giữa tính thực dụng và vẻ đẹp." />
+        <meta property="twitter:image" content="https://casa-vietnam.vn/assets/3d-rendering-contemporary-modern-dining-room-living-room-with-luxury-decor.jpg" />
+
       </Head>
       <body>
         <Main />
