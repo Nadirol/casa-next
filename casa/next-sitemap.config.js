0943770ff33d732494cca62d99da1d8bb18b1ed9
Khanh Duy <khanhduycb1510@gmail.com>
2023-10-10 11:25:27 +0700

    Update next-sitemap.config.js

--- casa/next-sitemap.config.js
+++ casa/next-sitemap.config.js
@@ -3,4 +3,5 @@ module.exports = {
     siteUrl: process.env.SITE_URL || 'https://casa-vietnam.vn',
     generateRobotsTxt: true, // (optional)
     // ...other options
+    baseUrl: 'https://casa-vietnam.vn/vi'
   }
